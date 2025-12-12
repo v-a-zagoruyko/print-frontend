470d9b2723c3abbd06cf83b5429e18939a77ee6c
Viktor Zagoruyko <vzagoruyko@rbc.ru>
2025-12-12 13:47:26 +0500

    Update Navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,6 +2,8 @@ import { Container, Navbar, Nav } from "solid-bootstrap";
 import { A } from "@solidjs/router";
 import { useInfo } from "utils/InfoContext";
 
+const LOGIN_URL = import.meta.env.VITE_LOGIN_URL;
+
 export function MainLayout(props) {
     const info = useInfo();
 
@@ -11,20 +13,23 @@ export function MainLayout(props) {
                 <Container>
                     <Navbar.Brand>
                         <Nav.Link as={A} href="/">
-                            {info.name}
+                            {info.company_name} {info.is_superuser}
                         </Nav.Link>
                     </Navbar.Brand>
 
                     <Navbar.Toggle aria-controls="main-navbar-collapse" />
 
                     <Navbar.Collapse id="main-navbar-collapse">
-                        <Nav class="ms-auto">
+                        <Nav class="me-auto">
                             <Nav.Link as={A} href="/print/products">
                                 Этикетки товаров
                             </Nav.Link>
                             <Nav.Link as={A} href="/print/contractors">
                                 Этикетки контрагентов
                             </Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Navbar.Text><a href={LOGIN_URL}>{info.username}</a></Navbar.Text>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
