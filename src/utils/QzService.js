48e0de18bf608bf465bda9b41f9fde92063de5c4
Viktor Zagoruyko <vzagoruyko@rbc.ru>
2025-12-25 17:31:37 +0500

    Test printing

--- src/utils/QzService.js
+++ src/utils/QzService.js
@@ -51,7 +51,7 @@ export async function printPdfBase64(base64Pdf, copies = 1, printerName = "") {
       format: "pdf",
       flavor: "base64",
       data: base64Pdf,
-      options: { ignoreTransparency: true, altFontRendering: true }
+      options: { ignoreTransparency: true }
     },
   ];
   await qz.print(config, data);
